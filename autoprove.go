503bfb3cb52918797f471bfd190ce44152b12911
lfaynman <faynman@ms5.hinet.net>
2020-03-20 11:41:59 +0800

    Update autoprove.go

--- autoprove.go
+++ autoprove.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
+	"log"
+	"math"
 	"os"
 	"os/exec"
 	"strconv"
@@ -13,89 +15,144 @@ import (
 
 const phoneSetPath string = "/usr/local/voip/ipphone/"
 
-var phoneType = []string{"x3s", "c58p", "168ge"}
-var xlsPhoneType,
-	xlsMac []string
+var supportPhoneType = []string{"x3s", "c58p", "168ge"}
 
-func ReadExcelData(column string) []string {
-
-	var data []string
-
-	//固定讀取上傳後的execl檔案
-	xlsx, err := excelize.OpenFile("/tmp/Extensions_data.xlsx")
+func main() {
+	orignalTime := time.Now().Unix()
+	var config, setPhone []string
+	path, err := exec.Command("/bin/sh", "-c", `/bin/cat /etc/xinetd.d/tftp |grep server_args|awk -F" " '{print $4}'`).Output()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
+		writeToLog(err.Error())
+		os.Exit(1)
 	}
-
-	sheetName := xlsx.GetSheetMap()[1]
-
-	//計算總筆數
-	rows, err := xlsx.GetRows(sheetName)
+	tftpOutPutPath := strings.Join(strings.Fields(string(path)), "") + "/"
+	command := "/bin/rm -f " + tftpOutPutPath + "*"
+	exec.Command("/bin/sh", "-c", command).Output()
+	exec.Command("/bin/sh", "-c", "true > /usr/local/voip/ipphone/autoprovlog.log").Output()
+	f, err := excelize.OpenFile("/tmp/Extensions_data.xlsx")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
+		writeToLog(err.Error())
+		return
 	}
-
-	var totalRecord int
-	for index, _ := range rows {
-		totalRecord = index + 1
+	phoneData, err := f.GetRows(f.GetSheetMap()[1])
+	if err != nil {
+		writeToLog(err.Error())
+		return
 	}
+	isMobile := covertExcelItemToArrayItem("A")
+	isUseAutoProvisioning := covertExcelItemToArrayItem("K")
+	usePhoneType := covertExcelItemToArrayItem("AY")
+	macAddress := covertExcelItemToArrayItem("AZ")
+	useSetPhoneTemplate := covertExcelItemToArrayItem("BA")
+	phoneData = makeFormateRegular(phoneData[2:], isMobile, isUseAutoProvisioning, usePhoneType, macAddress)
 
-	for i := 3; i <= totalRecord; i++ {
-		excelData, err := xlsx.GetCellValue(sheetName, column+strconv.Itoa(i))
-		if err != nil {
-			fmt.Println(err)
+	/*	ch := make(chan string, len(supportPhoneType))
+		for _, set := range supportPhoneType {
+			go func(set string) {
+				ch <- set
+			}(set)
+		}
+		for range supportPhoneType {
+			fmt.Println(<-ch)
+		}
+	*/
+
+	for i, _ := range phoneData {
+		if phoneData[i][isMobile] == "no" && phoneData[i][isUseAutoProvisioning] == "yes" && phoneData[i][usePhoneType] != "unknowtype" {
+			if i == 0 || !(phoneData[i][usePhoneType] == phoneData[i][usePhoneType-1] && phoneData[i][useSetPhoneTemplate] == phoneData[i][useSetPhoneTemplate-1]) && phoneData[i][isUseAutoProvisioning-1] == "yes" && phoneData[i][usePhoneType-1] != "unknowtype" {
+				config = config[:0]
+				lines, err := readLines(phoneSetPath + phoneData[i][usePhoneType] + "/" + phoneData[i][useSetPhoneTemplate])
+				if err != nil {
+					writeToLog("readLines: %s" + err.Error())
+				}
+				for _, line := range lines {
+					config = append(config, line)
+				}
+			}
+
+			if i == 0 || !(phoneData[i][useSetPhoneTemplate] == phoneData[i][useSetPhoneTemplate-1]) && phoneData[i][isUseAutoProvisioning-1] == "yes" && phoneData[i][usePhoneType-1] != "unknowtype" {
+				setPhone = setPhone[:0]
+				lines, err := readLines(phoneSetPath + phoneData[i][usePhoneType] + "/setphone")
+				if err != nil {
+					writeToLog("readLines: %s" + err.Error())
+				}
+
+				for _, line := range lines {
+					setPhone = append(setPhone, line)
+				}
+			}
+		}
+		for _, item := range setPhone {
+			var configTargetItem int
+			var phoneSetValue string
+			setPhoneSearchString := strings.Join(strings.Split(item, ":")[:1], "")
+			setPhoneData := strings.Split(strings.Join(strings.Split(item, ":")[1:2], ""), ",")
+			for _, data := range setPhoneData {
+				phoneSetValue = phoneSetValue + strings.Replace(data, strings.Join(strings.Fields(data), ""), phoneData[i][covertExcelItemToArrayItem(strings.Join(strings.Fields(data), ""))], -1)
+			}
+			typeFound := false
+			for i, configItem := range config {
+				if strings.Contains(configItem, setPhoneSearchString) {
+					typeFound = true
+					configTargetItem = i
+					continue
+				}
+			}
+			if typeFound == false {
+				writeToLog("The setPhone file Search no mach with setphone item , check the item is correct")
+			}
+			config[configTargetItem] = strings.Join(strings.SplitAfter(config[configTargetItem], ":")[:1], "") + phoneSetValue
 		}
 
-		switch column {
-		case "AZ":
-			{
-				replacer := strings.NewReplacer(":", "", "-", "")
-				data = append(data, strings.ToLower(replacer.Replace(excelData)))
-			}
-		case "A", "K", "BA":
-			{
-				data = append(data, strings.ToLower(excelData))
-			}
-		case "AY":
-			{
-				typeFound := false
-				for _, pType := range phoneType {
-					if strings.Contains(strings.ToLower(excelData), pType) {
-						typeFound = true
-						data = append(data, pType)
-						continue
-					}
-				}
-				if typeFound == false {
-					fmt.Println("Check The Phone Type had not provider")
-					data = append(data, "notype")
-				}
+		switch phoneData[i][usePhoneType] {
+		case "168ge":
+			exec.Command("/bin/sh", "-c", `mkdir -p /home/demo/public_html/DPH168GE/`).Output()
+			if err := writeLines(config, "/home/demo/public_html/DPH168GE/"+strings.ToUpper(phoneData[i][macAddress])+".dat"); err != nil {
+				writeToLog("Writeconfigfile Error : " + err.Error())
+
 			}
 		default:
-			data = append(data, excelData)
+			if err := writeLines(config, tftpOutPutPath+phoneData[i][macAddress]+".cfg"); err != nil {
+				writeToLog("Writeconfigfile Error : " + err.Error())
+			}
 		}
 	}
-	return data
+	writeToLog("Total " + strconv.Itoa(len(phoneData)) + "  Execel items maked.")
+	writeToLog("Total RunTime is  " + strconv.Itoa(int(time.Now().Unix()-orignalTime)) + "  sec.")
 }
 
-func GetPhoneSetValue(index int, setPhone string) (string, string) {
-	//	fmt.Printf("phonetype %s  index [%d]\n",xlsPhoneType[index-1],index-1)
-	var pSetValue string
-	xlsx, err := excelize.OpenFile("/tmp/Extensions_data.xlsx")
-	if err != nil {
-		fmt.Println(err)
-	}
-	line := strings.Split(strings.ToUpper(strings.Join(strings.Split(setPhone, ":")[1:2], "")), ",")
-	for _, data := range line {
-		xlsSetValue, err := xlsx.GetCellValue(xlsx.GetSheetMap()[1], strings.Join(strings.Fields(data), "")+strconv.Itoa(index))
-		if err != nil {
-			fmt.Println(err)
+func covertExcelItemToArrayItem(s string) int {
+	var sum int
+	p := len(s) - 1
+	for _, r := range strings.ToUpper(s) {
+		sum = (int(r)-64)*int(math.Pow(26, float64(p))) + sum
+		if p > 0 {
+			p--
 		}
-		pSetValue = pSetValue + strings.Replace(data, strings.Join(strings.Fields(data), ""), xlsSetValue, -1)
 	}
-	return strings.Join(strings.Split(setPhone, ":")[:1], ""), pSetValue
+	return (sum - 1)
+}
+
+func makeFormateRegular(phoneData [][]string, isMobile, isUseAutoProvisioning, usePhoneType, macAddress int) [][]string {
+	replacer := strings.NewReplacer(":", "", "-", "")
+	for i, _ := range phoneData {
+		phoneData[i][macAddress] = strings.ToLower(replacer.Replace(phoneData[i][macAddress]))
+		phoneData[i][isMobile] = strings.ToLower(phoneData[i][isMobile])
+		phoneData[i][isUseAutoProvisioning] = strings.ToLower(phoneData[i][isUseAutoProvisioning])
+		typeFound := false
+		for _, pType := range supportPhoneType {
+			if strings.Contains(strings.ToLower(phoneData[i][usePhoneType]), pType) {
+				typeFound = true
+				phoneData[i][usePhoneType] = pType
+				continue
+			}
+		}
+		if typeFound == false {
+			writeToLog("The execl row " + strconv.Itoa(i+3) + " phone type is  '" + phoneData[i][usePhoneType] + "'  the type not yet suppert so check The Phone Type\n" + strconv.Itoa(i+3) + " " + strings.Join(phoneData[i], " "))
+			phoneData[i][usePhoneType] = "unknowtype"
+		}
+	}
+	return phoneData
 }
 
 func readLines(path string) ([]string, error) {
@@ -127,90 +184,12 @@ func writeLines(lines []string, path string) error {
 	return w.Flush()
 }
 
-func spinner(delay time.Duration) {
-	for {
-		for _, r := range `-\|/` {
-			fmt.Printf("\r%c ", r)
-			time.Sleep(delay)
-
-		}
-	}
-}
-
-func main() {
-
-	var config, setPhone []string
-	go spinner(100 * time.Millisecond)
-	xlsIsMobile := ReadExcelData("A")
-	xlsAutoprov := ReadExcelData("K")
-	xlsPhoneType = ReadExcelData("AY")
-	xlsMac = ReadExcelData("AZ")
-	xlsSetPhoneTemplate := ReadExcelData("BA")
-	//	xlsSetPhoneBindIp := ReadExcelData("BB")
-
-	path, err := exec.Command("/bin/sh", "-c", `/bin/cat /etc/xinetd.d/tftp |grep server_args|awk -F" " '{print $4}'`).Output()
+func writeToLog(logmessage string) {
+	f, err := os.OpenFile("/usr/local/voip/ipphone/autoprovlog.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatalf("error opening logfile: %v", err)
 	}
-	tftpOutPutPath := strings.Join(strings.Fields(string(path)), "") + "/"
-	command := "/bin/rm -f " + tftpOutPutPath + "*"
-	exec.Command("/bin/sh", "-c", command).Output()
-	for i, isMobile := range xlsIsMobile {
-		if isMobile == "no" && xlsAutoprov[i] == "yes" && xlsPhoneType[i] != "notype" {
-			if i == 0 || !(xlsPhoneType[i] == xlsPhoneType[i-1] && xlsSetPhoneTemplate[i] == xlsSetPhoneTemplate[i-1]) && xlsAutoprov[i-1] == "yes" && xlsPhoneType[i-1] != "notype" {
-				config = config[:0]
-				lines, err := readLines(phoneSetPath + xlsPhoneType[i] + "/" + xlsSetPhoneTemplate[i])
-				if err != nil {
-					fmt.Printf("readLines: %s", err)
-				}
-				for _, line := range lines {
-					config = append(config, line)
-				}
-			}
-
-			if i == 0 || !(xlsPhoneType[i] == xlsPhoneType[i-1]) && xlsAutoprov[i-1] == "yes" && xlsPhoneType[i-1] != "notype" {
-				setPhone = setPhone[:0]
-				lines, err := readLines(phoneSetPath + xlsPhoneType[i] + "/setphone")
-				if err != nil {
-					fmt.Printf("readLines: %s", err)
-				}
-
-				for _, line := range lines {
-					setPhone = append(setPhone, line)
-				}
-			}
-		}
-		for _, item := range setPhone {
-			var configTargetItem int
-			phoneSetItem, phoneSetValue := GetPhoneSetValue(i+3, item)
-			typeFound := false
-			for i, configItem := range config {
-				if strings.Contains(configItem, phoneSetItem) {
-					typeFound = true
-					configTargetItem = i
-					continue
-				}
-			}
-			if typeFound == false {
-				fmt.Println("Search no mach with setphone item , check the item is correct")
-			}
-			config[configTargetItem] = strings.Join(strings.SplitAfter(config[configTargetItem], ":")[:1], "") + phoneSetValue
-		}
-		switch xlsPhoneType[i] {
-		case "x3s", "c58p":
-
-			if err := writeLines(config, tftpOutPutPath+xlsMac[i]+".cfg"); err != nil {
-				fmt.Println("Writeconfigfile Error : ", err)
-			}
-		case "168ge":
-			exec.Command("/bin/sh", "-c", `mkdir -p /home/demo/public_html/DPH168GE/`).Output()
-			if err := writeLines(config, "/home/demo/public_html/DPH168GE/"+strings.ToUpper(xlsMac[i])+".dat"); err != nil {
-				fmt.Println("Writeconfigfile Error : ", err)
-
-			}
-		}
-
-	}
-
+	defer f.Close()
+	log.SetOutput(f)
+	log.Println(logmessage)
 }
